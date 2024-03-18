2ae826d4eb4e7395a356f4d44f028963f1a27ad6
Denis <denisprolubsikov8449@gmail.com>
2024-03-18 23:18:01 +0500

    init

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -9,5 +9,4 @@ export const routes = [
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
     
     },
-    
-    ];
+];
