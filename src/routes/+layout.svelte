7d97d4c94044b0f7646f2713064e1e2bc7680ae9
Denis <denisprolubsikov8449@gmail.com>
2024-03-18 22:36:54 +0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+        <div class="nav-links">
+            {#if link.isShow}
+            <a href={link.href} class="link">{link.title}</a>
+            {/if}
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!--Pages will be injected below-->
+    <slot></slot>
+</div>
+
+<style>
+    .container{
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
+
+<script>
+    const nav = [
+        {
+            title: 'Обо мне',
+            href:'/about',
+            isShow: true,
+        },
+        {
+            title: 'Блог',
+            href:'/blog',
+            isShow: true,
+        },
+        {
+            title: 'Портфолио',
+            href:'/project',
+            isShow: true,
+        },
+        {
+            title: 'Контакты',
+            href:'/contact',
+            isShow: true,
+        },
+        {
+            title: 'temp',
+            href:'/temp',
+            isShow: false,
+        },
+    ];
+</script>
